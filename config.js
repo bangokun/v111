3d230e7888743ff285f6a8a9b53c433dfa021826
bangokun <108464869+bangokun@users.noreply.github.com>
2022-07-18 21:52:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6288227758096'],
-  ['6288227758096'],
-  ['6288227758096', 'Owner', true]
+  ['6285738639969'],
+  ['6285738639969'],
+  ['6285738639969', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@FazhBoT'
-  var sticker_author = 'FazhBoT'
+  var sticker_name = '@Crash'
+  var sticker_author = 'Crash'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Fachri'
+global.wm = '© 🤡'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
